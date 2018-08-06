4e29a6bb7988ccd4be6154d0182e662e1ad0c255
Eternal-Ben <lst.thierry@gmail.com>
2018-08-06 15:36:51 +0200

    Exo-LINQ Va servir pour la selection dans une liste ou des listes

---
+++ EXo-Linq/EXo-Linq/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXo_Linq
+{
+    class Program //Ville
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Ville
+        {
+            public string NomVille { get; set; }
+            public string CodePostal { get; set; }
+            public string Departement
+            {
+                get { return this.CodePostal.Substring(0, 2); }
+
+        public class Departement
+            {
+                public string NomDept { get; set; }
+                public string Numero { get; set; }
+            }
+            public class RequesteAvecGroupement ()
+            { AfficherEntete();
+            // Syntaxe de requete
+            var requete = from prenom in prenoms
+                          group prenom by prenom[0] into groupe
+                          select new
+                          {
+                              Lettre = groupe.Key,
+                              Prenoms = groupe.TolList()
+                          };
+                foreach (var resultat in requete)
+                {
+                Console.WriteLine(resultat.Lettre);
+                foreach(var prenom in resultat.Prenoms)
+                }
+
+        //Syntaxe de methode
+        public class RequesteAvecGroupement ()
+            var requete2 = prenoms
+                .OrderBy(prenom=> prenom)
+                .GroupBy(prenom=> prenom[0];
+                    .Select(groupe=> new
+                        {
+                            Lettre = groupe.Key,
+                            Prenoms = groupe.ToList
+                        }
+    }
+}
+}

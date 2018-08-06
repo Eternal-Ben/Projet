b72b816daca5e519b4e402ea58e9756279634db1
Eternal-Ben <lst.thierry@gmail.com>
2018-08-06 10:48:57 +0200

    Travail sur les enum

---
+++ Enumeration/Enumeration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    public JourSemaine {get;set;}
+        }
+{public enum JourSemaine
+{
+    Lundi = 1,
+    Mardi = 2,
+    Mercredi = 3,
+    Jeudi = 4,
+    Vendredi = 5,
+    samedi = 6,
+    Dimanche = 7,
+
+}
+    }
+}

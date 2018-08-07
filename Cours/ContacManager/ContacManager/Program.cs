210688584137f790ed4d71153a9f52f6e8534958
Eternal-Ben <lst.thierry@gmail.com>
2018-08-07 10:39:55 +0200

    Ajout d'instruction  et d'un nouveau choix de menu

--- Cours/ContacManager/ContacManager/Program.cs
+++ Cours/ContacManager/ContacManager/Program.cs
@@ -366,18 +366,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ContacManager
-{
+namespace ContactManager
+/*{
     class Program
     {
+        static List<string> contacts = new List<string>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenu dans votre gestionnaire de contact");
-            Console.WriteLine("Menu");
-//            Console.WriteLine("1 * Liste de vos contacts");
-//            Console.WriteLine("2 * Ajout d'un contact dans votre repertoir");
-//            Console.WriteLine("3 * Suprimer un contact de votre repertoir");
-//            Console.WriteLine("4 * Quitter");
+            var choix = AfficherMenu();
+            switch (choix)
+            {
+                case "1":
+                    ListerContacts();
+                    break;
+                case "2":
+                    AjouterContact();
+                    break;
+                case "3":
+                    SupprimerContact();
+                    break;
+                case "4":
+                    continuer = false;
+                    break;
+                default:
+                    Console.WriteLine("Choix invalide, l'application va fermer...");
+                    continuer = false;
+                    break;
+            }
+
+            //           Console.WriteLine("Bienvenu dans votre gestionnaire de contact");
+            //           Console.WriteLine("Menu");
+            //            Console.WriteLine("1 * Liste de vos contacts");
+            //            Console.WriteLine("2 * Ajout d'un contact dans votre repertoir");
+            //            Console.WriteLine("3 * Suprimer un contact de votre repertoir");
+            //            Console.WriteLine("4 * Quitter");
 
         }
         static void ListeContacts()
@@ -401,5 +424,160 @@ namespace ContacManager
 // revenir ensuite au Choix du menu
         }
 
+    }
+}*/
+/*Sup de Code
+* Mod le choix pr qu'il corresponde à "Q" et plus à "4"
+* Ajouter une nouvelle entree dans le menu: "4.Trier contacts"
+* > Cette fx offre un choix sur la colonne trier:
+*  1 = par nom
+*  2 = par prenom
+* > Puis on liste tous les contacts triées
+* 
+* Ajouter une nouvelle entree dans le menu: "5. Filtrer contacts"
+* > Cette fx demande à l'utilisateur de saisir un txt
+* > Ensuite on affiche les contacts dont le nom ou le prenom commencent par le txt saisi par le use
+* (astuce) utiliser la methode StartsWith de la classe string
+*/
+{
+    class Program
+    {
+        static List<string> contacts = new List<string>();
+
+        static void Main(string[] args)
+        {
+            bool continuer = true;
+            while (continuer)
+            {
+                var choix = AfficherMenu();
+                switch (choix)
+                {
+                    case "1":
+                        ListerContacts();
+                        break;
+                    case "2":
+                        AjouterContact();
+                        break;
+                    case "3":
+                        SupprimerContact();
+                        break;
+                    //* Ajouter une nouvelle entree dans le menu: "4.Trier contacts"
+                    case "4":
+                        TrierContact();
+                        break;
+
+                    case "5":
+                       // FiltrerContact();
+                        break;
+
+                    // Mod le choix pr qu'il corresponde à "Q" et plus à "4"
+                    case "Q":
+                        continuer = false;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Choix invalide, l'application va fermer...");
+                        continuer = false;
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Affiche le menu
+        /// </summary>
+        /// <returns>Retourne le choix de l'utilisateur.</returns>
+        static string AfficherMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("MENU\n");
+            Console.WriteLine("1. Liste des contacts");
+            Console.WriteLine("2. Ajout d’un contact");
+            Console.WriteLine("3. Suppression d’un contact");
+            Console.WriteLine("4. Trier contacts");
+            Console.WriteLine("5. Filtrer contacts");
+            Console.WriteLine("Q");
+            Console.Write("\nVotre choix: ");
+
+            return Console.ReadLine();
+        }
+
+        static void ListerContacts()
+        {
+            Console.Clear();
+            Console.WriteLine("LISTE DES CONTACTS\n");
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"- {contact}");
+            }
+
+            Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+            Console.ReadKey();
+        }
+
+        static void AjouterContact()
+        {
+            Console.Clear();
+            Console.WriteLine("AJOUT D'UN CONTACT\n");
+
+            Console.WriteLine("Entre le nom:");
+            var contact = Console.ReadLine();
+            contacts.Add(contact);
+
+            Console.WriteLine("Contact ajouté !");
+
+            Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+            Console.ReadKey();
+        }
+
+        static void TrierContact()
+        {
+            OutilsConsole.AfficherMessageInconnu
+            var choixTri = Console.ReadLine();
+            byte tri;
+            while (!byte.TryParse(saisie, out tri)&& (tri <1||tri>2))
+
+            Console.WriteLine("TRIER CONTACT\n");
+
+            Console.WriteLine("Par nom:");
+            var contact = Console.ReadLine();
+            contacts.Add(contact);
+
+            Console.WriteLine("Par prenom !");
+
+            Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+            Console.ReadKey();
+        }
+
+
+        static void SupprimerContact()
+        {
+            Console.Clear();
+            Console.WriteLine("SUPPRESSION D'UN CONTACT\n");
+
+            for (var i = 0; i < contacts.Count; i++)
+            {
+                Console.WriteLine($"- {contacts[i]} ({i})");
+            }
+
+            Console.Write("Entre le numéro du contact à supprimer: ");
+            var index = int.Parse(Console.ReadLine());
+
+            if (index < contacts.Count)
+            {
+                contacts.RemoveAt(index);
+                Console.WriteLine("Contact supprimé !");
+            }
+            else
+            {
+                Console.WriteLine("Numéro invalide !");
+            }
+
+            Console.WriteLine("\nAppuie sur une touche pour revenir au menu...");
+            Console.ReadKey();
+        }
     }
 }

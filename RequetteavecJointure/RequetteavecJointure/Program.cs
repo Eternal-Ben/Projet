2572fffb4eee7701e01f069a24397fcabf8b0ecd
Eternal-Ben <lst.thierry@gmail.com>
2018-08-07 11:54:06 +0200

    requette avec jointure a finaliser

---
+++ RequetteavecJointure/RequetteavecJointure/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequetteavecJointure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// requete
+            var requete = from ville in villes
+                          join departement in departements
+                              on ville.Departement equals deparement.Numero
+                                  into jointure
+                          from ligne in jointure.DefaultIfEmpty()
+                          select new
+                          {
+                              Ville = ville.nom,
+                              NomDepartement = departement.Nom
+                          };
+            foreach (var resultat in requete)
+            {
+                Console.WriteLine($" - {resultat.ville} ({resultat.Nomdepartement})");
+            }
+        }
+    }
+}
